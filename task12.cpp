a2a8c7ada3c86b914b4c4155ee55568c349bb072
naiduik-maxim <maks.naud@gmail.com>
2025-04-02 15:57:58 +0300

    second commit

--- task12.cpp
+++ task12.cpp
@@ -29,18 +29,21 @@ public:
     int get_value() {return value;}
     bool get_fixed() {return fixed;}
     bool get_connected() {return connected;}
+    void set_connected() {connected = true;}
+    void set_value(int v) {value = v;}
 };
 
-bool can_place(int row, int col, cell obj);
+bool can_place(int row, int col);
 int find_max_fixed_number(cell field[ROW][COL], int max_number, int &row, int &col);
 bool find_same_fixed_number_BFS(cell field[ROW][COL], int value, int start_row, int start_col);
+bool connect_path(cell field[ROW][COL], int value, int start_row, int start_col, int finish_row, int finish_col);
 void solve(cell field[ROW][COL]);
 void print_field(cell field[ROW][COL]);
 
 int main(){
     cell field[ROW][COL] = {cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(),
                             cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(),
-                            cell(1), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(19),
+                            cell(1), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(19), cell(19),
                             cell(5), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(19),
                             cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(),
                             cell(1), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(), cell(19),
@@ -49,15 +52,18 @@ int main(){
                             cell(), cell(), cell(5), cell(1), cell(), cell(), cell(), cell(14), cell(14), cell(), cell(),};
     
     cout <<" Starting field" << endl;
-    print_field(field);   
+    print_field(field);
 
+    solve(field);
+
+    cout <<" Solved field" << endl;
+    print_field(field);
     return 0;
 }
 
-bool can_place(int row, int col, cell obj){
+bool can_place(int row, int col){
     if(row < 0 || col < 0) return false;
-    else if(row > ROW || col > 0) return false;
-    else if(obj.get_fixed()) return false;
+    else if(row >= ROW || col >= COL) return false;
     else return true; 
 }
 
@@ -90,26 +96,112 @@ bool find_same_fixed_number_BFS(cell field[ROW][COL], int value, int start_row, 
     search.push(coord(start_row, start_col));
     visited[start_row][start_col] = true;
 
-    coord currect = search.front();
-    search.pop();
-    while(currect.x + currect.y < value){
-        currect = search.front();
+    while(!search.empty()){
+        coord currect = search.front();
         search.pop();
+
         for(int i = 0; i < 4; i++){
             int new_row = currect.x + dir[i][0];
             int new_col = currect.y + dir[i][1];
 
-            if((new_row >= 0 && new_row < ROW && new_col >= 0 && new_col < COL) && !visited[new_row][new_col]){
+            cout << start_row << " , " << start_col << " -> " << new_row << " , " << new_col << endl;
+
+            if((abs(start_row - new_row) + abs(start_col - new_col)) > value) continue;
+
+            if(can_place(new_row, new_col) && !visited[new_row][new_col]){
                 visited[new_row][new_col] = true;
                 search.push(coord(new_row, new_col));
+                
+                if(field[new_row][new_col].get_value() == value &&
+                   field[new_row][new_col].get_fixed() &&
+                   !field[new_row][new_col].get_connected() &&
+                   (new_row != start_row || new_col != start_col)){
 
-                if(field[new_row][new_col].get_value() == value && !field[new_row][new_col].get_connected()){
-                    //Створити метод для з'єднання 
+                    if(connect_path(field, value, start_row, start_col, new_row, new_col)){
+                        field[start_row][start_col].set_connected();
+                        field[new_row][new_col].set_connected();
+                    
+                        return true;
+                    }
                 }
             }
         }
     }
+
+    return false;
 }
+
+bool connect_path(cell field[ROW][COL], int value, int start_row, int start_col, int finish_row, int finish_col) {
+    if(start_row == finish_row){
+        int col_start = min(start_col, finish_col);
+        int col_end = max(start_col, finish_col);
+        for(int col = col_start + 1; col < col_end ; col++){
+            if(field[start_row][col].get_fixed() && field[start_row][col].get_value() != value) return false;
+            field[start_row][col].set_value(value);
+        }
+        return true;
+    }
+
+    if (start_col == finish_col) {
+        int row_start = min(start_row, finish_row);
+        int row_end = max(start_row, finish_row);
+        for (int row = row_start + 1; row < row_end; row++) {
+            if (field[row][start_col].get_fixed() && field[row][start_col].get_value() != value) return false; 
+            field[row][start_col].set_value(value);
+        }
+        return true;
+    }
+
+    if (!field[start_row][finish_col].get_fixed() || field[start_row][finish_col].get_value() == value) {
+        bool through_col = true;
+        for (int col = min(start_col, finish_col) + 1; col < max(start_col, finish_col); col++) {
+            if (field[start_row][col].get_fixed() && field[start_row][col].get_value() != value) {
+                through_col = false;
+                break;
+            }
+            field[start_row][col].set_value(value);
+        }
+        if (through_col) {
+            for (int row = min(start_row, finish_row); row <= max(start_row, finish_row); row++) {
+                if (field[row][finish_col].get_fixed() && field[row][finish_col].get_value() != value) return false;
+                field[row][finish_col].set_value(value);
+            }
+            return true;
+        }
+    }
+
+    if (!field[finish_row][start_col].get_fixed() || field[finish_row][start_col].get_value() == value) {
+        bool through_row = true;
+        for (int row = min(start_row, finish_row) + 1; row < max(start_row, finish_row); row++) {
+            if (field[row][start_col].get_fixed() && field[row][start_col].get_value() != value) {
+                through_row = false;
+                break;
+            }
+            field[row][start_col].set_value(value);
+        }
+        if (through_row) {
+            for (int col = min(start_col, finish_col); col <= max(start_col, finish_col); col++) {
+                if (field[finish_row][col].get_fixed() && field[finish_row][col].get_value() != value) return false;
+                field[finish_row][col].set_value(value);
+            }
+            return true;
+        }
+    }
+    return false; 
+}
+
+
+void solve(cell field[ROW][COL]){
+    int x;
+    int y;
+    int max = 0;
+    while((max = find_max_fixed_number(field, max, x, y)) != 1){
+        cout << max << endl;
+        while(find_same_fixed_number_BFS(field, max, x,y)){
+
+        }
+    }
+}   
 
 void print_field(cell field[ROW][COL]){
     for(int i = 0; i < ROW; i++){
